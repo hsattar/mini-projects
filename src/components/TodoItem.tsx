64b6d258d7a6d69d8525acf103dd9518b5381271
Hasan Sattar <hasanraza1798@gmail.com>
2022-02-05 21:55:58 +0000

    added undo functionality but not working?

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,7 +1,16 @@
-export default function TodoItem() {
+interface Props {
+    name: string
+    description: string
+    index: number
+}
+
+export default function TodoItem({ name, description, index }: Props) {
     return (
         <>
-            
+            <h4>Task {index + 1}</h4>
+            <h4>{name}</h4>
+            <h6>{description}</h6>
+            <hr />
         </>
     )
 }
