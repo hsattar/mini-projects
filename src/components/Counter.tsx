2b368d4a549bea5de261197db3f40d4cbe699d8f
Hasan Sattar <hasanraza1798@gmail.com>
2022-02-05 20:55:20 +0000

    counter undo and redo works

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,18 +1,22 @@
 import { useDispatch, useSelector } from "react-redux"
 import { decreaseCounterAction, increaseCounterAction } from "../redux/actions"
 import { IReduxStore } from "../types"
-  
+import { ActionCreators } from "redux-undo"
+
 
 export default function Counter() {
 
     const dispatch = useDispatch()
     const count = useSelector((state: IReduxStore) => state.counter.present.count)
+    const { undo, redo } = ActionCreators
     
     return (
         <>
         <h1>{count}</h1>
         <button onClick={() => dispatch(increaseCounterAction())}>Increment</button>
         <button onClick={() => dispatch(decreaseCounterAction())}>Decrement</button>
+        <button onClick={() => dispatch(undo())}>Undo</button>
+        <button onClick={() => dispatch(redo())}>Redo</button>
         </>
     )
 }
